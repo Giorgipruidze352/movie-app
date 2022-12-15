57080e8177698fd17a8346bdff18b790abffcd9e
Giorgipruidze352 <giorgi.pruidze352@ens.tsu.ge>
2022-12-15 20:19:31 +0400

    added router context and app routes component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {BrowserRouter as Router } from "react-router-dom"
+import AppRoutes from "./components/AppRoutes";
 import Navigation from "./components/Navigation";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navigation />
-    </>
+      <AppRoutes />
+    </Router>
   );
 };
 
