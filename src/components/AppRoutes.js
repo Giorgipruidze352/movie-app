03079654cb2e58d51456894b2522698599650fbf
Giorgipruidze352 <giorgi.pruidze352@ens.tsu.ge>
2022-12-16 19:13:09 +0400

    added routes for signup and login pages

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Favorites from "./Favorites";
+import Login from './Login';
+import Register from './Register';
 
 const AppRoutes = () => {
   return (
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="favorites" element={<Favorites />} />
-      </Routes>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="home" element={<Home />} />
+      <Route path="favorites" element={<Favorites />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Register />} />
+    </Routes>
   );
 };
 
