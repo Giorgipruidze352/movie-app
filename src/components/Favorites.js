60cfd6bb2de27061b0fb75405c84621f3e916e5d
Giorgipruidze352 <giorgi.pruidze352@ens.tsu.ge>
2022-12-17 17:28:22 +0400

    show content on favorites page

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,5 +1,37 @@
+import { Grid, Typography } from "@mui/material";
+import Movie from "./Movie";
+import { useContext } from "react";
+import MovieContext from "../contexts/Movie";
+
 const Favorites = () => {
-  return <h1>Favorites</h1>;
+  const { favorites, addToFavorites, removeMovie } = useContext(MovieContext);
+  return (
+    <>
+      {favorites.length > 0 ? (
+        <Grid container direction="column" sx={{ p: 4 }}>
+          {favorites.map((movie) => (
+            <Grid key={movie.id} item sx={{ pb: 4 }}>
+              <Movie
+                movie={movie}
+                addToFavorites={addToFavorites}
+                removeMovie={removeMovie}
+                favorites={favorites}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          textAlign="center"
+          m={2}
+        >
+          No Favorites yet
+        </Typography>
+      )}
+    </>
+  );
 };
 
 export default Favorites;
